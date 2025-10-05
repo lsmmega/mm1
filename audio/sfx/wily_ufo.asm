2d0107193f4fdb04124d4f8e67f414a36de5cfb3
lsmmega <pogmegalsm@outlook.com>
2025-10-05 12:56:07 +0800

    sfx wily ufo done

---
+++ audio/sfx/wily_ufo.asm
@@ -0,0 +1,22 @@
+sfx_wily_ufo:
+	sfx_priority 13
+	.BYTE sfx_pulse_1 | sfx_pulse_2
+
+;1
+;sfx_pulse_1
+@loop_1:
+	duty_cycle 1
+	pitch_envelope $FF
+	sfx_modulator_set 1, 2, 0, volume_decay, 0, 0
+	volume 15
+	sfx_note nAb5
+	sfx_frames 16
+
+;sfx_pulse_2
+	duty_cycle 1
+	pitch_envelope $FF
+	sfx_modulator_set 1, 2, 0, volume_decay, 0, 0
+	volume 10
+	sfx_note nDb6
+	loop 35, @loop_1
+	sfx_end
