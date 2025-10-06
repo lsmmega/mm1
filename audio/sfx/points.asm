c5e13d80b06261a104323aa75e23500d658f9e7e
lsmmega <pogmegalsm@outlook.com>
2025-10-06 17:59:45 +0800

    sfx points done

---
+++ audio/sfx/points.asm
@@ -0,0 +1,18 @@
+sfx_points:
+	sfx_priority 10
+	.BYTE sfx_pulse_2
+
+;1
+;sfx_pulse_2
+	sfx_frames 4
+	volume 15
+	duty_cycle 1
+	sfx_note nG_4
+
+;2
+;sfx_pulse_2
+	sfx_frames 4
+	volume 15
+	duty_cycle 1
+	sfx_note nA_4
+	sfx_end
