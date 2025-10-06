20fb28d28b18873379757e74e9d01ca89d9c160d
lsmmega <pogmegalsm@outlook.com>
2025-10-06 11:32:51 +0800

    rework

--- audio/sfx/magnet_beam.asm
+++ audio/sfx/magnet_beam.asm
@@ -9,12 +9,12 @@ sfx_magnet_beam:
 	sfx_modulator_set 7, 1, 1, volume_decay, 1, 7
 	sfx_note nGb3
 
+;2
 ;sfx_noise
 	noise_duty_cycle 1
 	sfx_modulator_set 7, 1, 1, volume_decay, 1, 7
 	sfx_noise_note 10
 
-;2
 ;sfx_pulse_2
 	sfx_frames 24
 	duty_cycle 1
@@ -22,8 +22,9 @@ sfx_magnet_beam:
 	sfx_modulator_set 7, 1, 1, volume_decay, 1, 7
 	sfx_note nA_3
 
-;sfx_noise
-	noise_duty_cycle 1
+;3
+;sfx_pulse_2
+	duty_cycle 2
 	sfx_modulator_set 7, 1, 1, volume_decay, 1, 7
-	sfx_noise_note 11
+	sfx_note $0004
 	sfx_end
