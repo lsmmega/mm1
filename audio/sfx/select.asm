b97a72e308895fb0cca20a6099e3c9a3bc70a61a
lsmmega <pogmegalsm@outlook.com>
2025-10-06 17:54:19 +0800

    sfx select done

---
+++ audio/sfx/select.asm
@@ -0,0 +1,30 @@
+sfx_select:
+	sfx_priority 9
+	.BYTE sfx_pulse_1 | sfx_pulse_2
+
+;1
+;sfx_pulse_1
+	volume 15
+	duty_cycle 1
+	sfx_note nAb4 + 3
+
+;sfx_pulse_2
+	sfx_frames 5
+	volume 15
+	duty_cycle 3
+	sfx_note nAb4
+
+;2
+;sfx_pulse_1
+	volume 15
+	sfx_modulator_set 7, 0, 0, volume_decay, 0, 0
+	duty_cycle 1
+	sfx_note nDb4 + 3
+
+;sfx_pulse_2
+	sfx_frames 8
+	volume 15
+	sfx_modulator_set 7, 0, 0, volume_decay, 0, 0
+	duty_cycle 1
+	sfx_note nDb4
+	sfx_end
