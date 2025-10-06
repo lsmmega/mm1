31825f22a6976331903c16e7c22b089e31ac524d
lsmmega <pogmegalsm@outlook.com>
2025-10-06 18:14:46 +0800

    sfx jump into water done

---
+++ audio/sfx/jump_into_water.asm
@@ -0,0 +1,28 @@
+sfx_jump_into_water:
+	sfx_priority 12
+	.BYTE sfx_pulse_2 | sfx_noise
+
+;1
+;sfx_pulse_2
+	pitch_envelope $60
+	sfx_modulator_set 1, 1, 1, volume_decay, 5, 2
+	volume 9
+	sfx_note nA_2
+
+;sfx_noise
+	sfx_frames 6
+	volume 15
+	sfx_noise_note 8
+
+;2
+;sfx_pulse_2
+	pitch_envelope $30
+	sfx_modulator_set 2, 1, 1, volume_decay, 5, 2
+	volume 9
+	sfx_note nB_5
+
+;sfx_noise
+	sfx_frames 23
+	volume 15
+	sfx_noise_note 12
+	sfx_end
