d3d530cb854b962c20965d8d703419cd659fe71d
lsmmega <pogmegalsm@outlook.com>
2026-02-21 13:14:32 +0800

    combine

--- home/megaman_death.asm
+++ home/megaman_death.asm
@@ -94,3 +94,71 @@ _megaman_death:
 
 @no_life:
 	JMP _game_over
+
+checkpoint_1_active_room_table:
+	.BYTE $0A ;cutman
+	.BYTE $0A ;iceman
+	.BYTE $0E ;bombman
+	.BYTE $08 ;fireman
+	.BYTE $07 ;elecman
+	.BYTE $0A ;gutsman
+	.BYTE $22 ;wily1
+	.BYTE $24 ;wily2
+	.BYTE $2F ;wily3
+	.BYTE $1D ;wily4
+	.BYTE $1D ;unused
+	.BYTE $1D ;unused
+
+checkpoint_2_active_room_table:
+	.BYTE $14 ;cutman
+	.BYTE $14 ;iceman
+	.BYTE $14 ;bombman
+	.BYTE $10 ;fireman
+	.BYTE $14 ;elecman
+	.BYTE $0F ;gutsman
+	.BYTE $27 ;wily1
+	.BYTE $29 ;wily2
+	.BYTE $2F ;wily3
+	.BYTE $21 ;wily4
+	.BYTE $1C ;unused
+
+checkpoint_0_respawn_room_table:
+	.BYTE $00 ;cutman
+	.BYTE $00 ;iceman
+	.BYTE $00 ;bombman
+	.BYTE $00 ;fireman
+	.BYTE $00 ;elecman
+	.BYTE $00 ;gutsman
+	.BYTE $18 ;wily1
+	.BYTE $18 ;wily2
+	.BYTE $18 ;wily3
+	.BYTE $14 ;wily4
+	.BYTE $18 ;unused
+	.BYTE $1E ;unused
+
+;checkpoint 1 respawn room table
+	.BYTE $0A ;cutman
+	.BYTE $0A ;iceman
+	.BYTE $0E ;bombman
+	.BYTE $08 ;fireman
+	.BYTE $07 ;elecman
+	.BYTE $0A ;gutsman
+	.BYTE $22 ;wily1
+	.BYTE $24 ;wily2
+	.BYTE $2E ;wily3
+	.BYTE $1D ;wily4
+	.BYTE $1D ;unused
+	.BYTE $1D ;unused
+
+;checkpoint 2 respawn room table
+	.BYTE $14 ;cutman
+	.BYTE $14 ;iceman
+	.BYTE $14 ;bombman
+	.BYTE $10 ;fireman
+	.BYTE $14 ;elecman
+	.BYTE $0F ;gutsman
+	.BYTE $26 ;wily1
+	.BYTE $28 ;wily2
+	.BYTE $2E ;wily3
+	.BYTE $21 ;wily4
+	.BYTE $20 ;unused
